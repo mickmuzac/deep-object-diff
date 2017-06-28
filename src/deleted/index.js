4eb84608426a5b981f1cd41635edfbb89a752f9b
Mick Muzac <mmuzac23@gmail.com>
2017-06-28 17:01:56 -0400

    Update index.js

--- src/deleted/index.js
+++ src/deleted/index.js
@@ -7,7 +7,7 @@ const deletedDiff = (lhs, rhs) => {
   const r = properObject(rhs);
 
   return Object.keys(l).reduce((acc, key) => {
-    if (r.hasOwnProperty(key)) {
+    if (r.hasOwnProperty(key) && key !== 'private') {
       const difference = deletedDiff(l[key], r[key]);
 
       if (isObject(difference) && isEmpty(difference)) return acc;
